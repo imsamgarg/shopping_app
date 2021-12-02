aa7aee715cb155d0ec67af3228d0131434bae9b3
Sam Garg <smlgarg123@gmail.com>
2021-12-02 18:39:58 +0530

    assets added

---
+++ lib/app/core/values/assets.dart
@@ -0,0 +1,23 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesGLogo
+  /// assets/images/gLogo.png
+  static const String assetsImagesGLogo = "assets/images/gLogo.png";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.png
+  static const String assetsImagesLogo = "assets/images/logo.png";
+
+  /// Assets for assetsImagesLoveAnimatiom
+  /// assets/images/loveAnimatiom.riv
+  static const String assetsImagesLoveAnimatiom = "assets/images/loveAnimatiom.riv";
+
+  /// Assets for assetsImagesShape2
+  /// assets/images/shape2.png
+  static const String assetsImagesShape2 = "assets/images/shape2.png";
+
+  /// Assets for assetsImagesWelcome
+  /// assets/images/welcome.png
+  static const String assetsImagesWelcome = "assets/images/welcome.png";
+}
