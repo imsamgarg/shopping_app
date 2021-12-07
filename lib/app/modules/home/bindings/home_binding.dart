3cb4ba3bbf814e51b86b360f87ae9beab1cd7974
Sam Garg <smlgarg123@gmail.com>
2021-12-07 21:34:08 +0530

    home binding update

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
+import 'package:shopping_app/app/data/services/cart_service.dart';
+import 'package:shopping_app/app/data/services/config_service.dart';
+import 'package:shopping_app/app/data/services/user_service.dart';
 
-import 'package:shopping_app/app/modules/home/controllers/address_controller.dart';
 import 'package:shopping_app/app/modules/home/controllers/favourite_controller.dart';
 import 'package:shopping_app/app/modules/home/controllers/profile_controller.dart';
 
@@ -9,17 +11,14 @@ import '../controllers/home_controller.dart';
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<AddressController>(
-      () => AddressController(),
-    );
-    Get.lazyPut<FavouriteController>(
-      () => FavouriteController(),
-    );
-    Get.lazyPut<ProfileController>(
-      () => ProfileController(),
-    );
-    Get.lazyPut<HomeController>(
-      () => HomeController(),
-    );
+    //services
+    Get.put<UserService>(UserService());
+    Get.put<CartService>(CartService());
+    Get.put<ConfigService>(ConfigService());
+
+    //controllers
+    Get.put<FavouriteController>(FavouriteController());
+    Get.lazyPut<ProfileController>(() => ProfileController());
+    Get.lazyPut<HomeController>(() => HomeController());
   }
 }
