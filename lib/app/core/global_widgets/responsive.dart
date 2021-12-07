f3ca20473d2a975cd560be09ee4bcb487b8a6896
Sam Garg <smlgarg123@gmail.com>
2021-12-07 09:28:18 +0530

    responsive fix

--- lib/app/core/global_widgets/responsive.dart
+++ lib/app/core/global_widgets/responsive.dart
@@ -1,45 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/app/core/values/assets.dart';
-import 'package:shopping_app/app/core/values/values.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-class AdaptiveHide extends StatelessWidget {
-  final Widget child;
-  final ScreenSize hideOn;
+// class AdaptiveHide extends StatelessWidget {
+//   final Widget child;
+//   final ScreenSize hideOn;
+//   final double viewInsets;
 
-  const AdaptiveHide({Key? key, required this.hideOn, required this.child})
-      : super(key: key);
+//   const AdaptiveHide(
+//       {Key? key,
+//       required this.hideOn,
+//       required this.child,
+//       this.viewInsets = 0})
+//       : super(key: key);
 
-  Widget calculateSizeAndBuild(BuildContext c) {
-    final _screenSize = c.mq.size.height - c.mq.viewInsets.bottom;
-    if (hideOn == ScreenSize.sm && _screenSize >= sm) return child;
-    if (hideOn == ScreenSize.md && _screenSize >= md) return child;
-    if (hideOn == ScreenSize.lg && _screenSize >= lg) return child;
-    if (hideOn == ScreenSize.xl && _screenSize >= xl) return child;
-    return SizedBox();
-  }
+//   Widget calculateSizeAndBuild(BuildContext c) {
+//     final height = c.mq.size.height;
+//     final insets = c.mq.viewInsets.bottom;
+//     final _screenSize = height - viewInsets;
+//     if (hideOn == ScreenSize.sm && _screenSize >= sm) return child;
+//     if (hideOn == ScreenSize.md && _screenSize >= md) return child;
+//     if (hideOn == ScreenSize.lg && _screenSize >= lg) return child;
+//     if (hideOn == ScreenSize.xl && _screenSize >= xl) return child;
+//     return SizedBox();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return calculateSizeAndBuild(context);
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return calculateSizeAndBuild(context);
+//   }
+// }
 
 class HeaderImage extends StatelessWidget {
+  final double viewInsets;
+  final String? image;
   const HeaderImage({
     Key? key,
+    required this.viewInsets,
+    this.image,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AdaptiveHide(
-      hideOn: ScreenSize.lg,
-      child: Hero(
-        tag: "image",
-        child: Container(
-          child: Image.asset(
-            Assets.assetsImagesWelcome,
-          ),
+    return Hero(
+      tag: "image",
+      child: SizedBox(
+        height: (context.screenHeight - viewInsets) * 0.35,
+        child: Image.asset(
+          image ?? Assets.assetsImagesWelcome,
+          fit: BoxFit.contain,
         ),
       ),
     );
