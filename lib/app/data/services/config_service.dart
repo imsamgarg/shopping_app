b7a050a807697c68a1130176898da0f6039acbd1
Sam Garg <smlgarg123@gmail.com>
2021-12-07 21:32:59 +0530

    config service added

---
+++ lib/app/data/services/config_service.dart
@@ -0,0 +1,39 @@
+import 'package:get/get.dart';
+import 'package:shopping_app/app/core/values/db_strings.dart';
+import 'package:shopping_app/app/data/models/app_model.dart';
+import 'package:shopping_app/app/data/repository/database_repository.dart';
+
+class ConfigService extends GetxService {
+  late final _repo = FirebaseDbRepository();
+  static ConfigService service() => Get.find<ConfigService>();
+  // final Rxn<AppModel?> _data = Rxn<AppModel>(null);
+  // AppModel? get data => _data.value;
+  // set data(value) => _data.value = value;
+
+  AppModel? data;
+  Future<void> init() async {
+    if (data == null) {
+      await _getData();
+      // _subscribeData();
+    }
+    return;
+  }
+
+  // void _subscribeData() {
+  //   _repo.subscribeToDocument(Db.configDocument, Db.configCol).listen(
+  //     (event) {
+  //       final doc = event.data();
+  //       data = AppModel.fromJson(doc);
+  //     },
+  //   );
+  // }
+
+  Future<void> _getData() async {
+    final doc = await _repo.getDocumentFromFirestore(
+      Db.configDocument,
+      Db.configCol,
+    );
+    data = AppModel.fromJson(doc);
+    return;
+  }
+}
