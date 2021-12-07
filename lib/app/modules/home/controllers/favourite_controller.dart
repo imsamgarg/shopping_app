1f1f09f8180c839397ab4f57a5af26b8ff26db90
Sam Garg <smlgarg123@gmail.com>
2021-12-07 21:54:07 +0530

    favourite_controller done

--- lib/app/modules/home/controllers/favourite_controller.dart
+++ lib/app/modules/home/controllers/favourite_controller.dart
@@ -1,13 +1,24 @@
 import 'package:get/get.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:shopping_app/app/core/utils/mixins.dart';
+import 'package:shopping_app/app/data/models/product_model.dart';
 
 class FavouriteController extends GetxController with ServicesMixin {
   static const _fetchCount = 10;
-  late final pagingController = PagingController(firstPageKey: 0)
-    ..addPageRequestListener((pageKey) {
-      fetchProducts(pageKey);
-    });
+  late final PagingController<int, ProductModel> pagingController;
+
+  @override
+  void onInit() {
+    pagingController = PagingController<int, ProductModel>(firstPageKey: 0);
+    pagingController.addPageRequestListener(fetchProducts);
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    pagingController.dispose();
+    super.onClose();
+  }
 
   Future<void> fetchProducts(int pageKey) async {
     final products = await userService.getFavouriteProducts(
@@ -20,5 +31,27 @@ class FavouriteController extends GetxController with ServicesMixin {
     } else {
       pagingController.appendPage(products, pageKey + products.length);
     }
+  }
+
+  void addFavouriteProduct(ProductModel product) {
+    final list = pagingController.value.itemList ?? [];
+    final newList = [product, ...list];
+    pagingController.value = PagingState(
+      itemList: newList,
+      nextPageKey: newList.length,
+    );
+  }
+
+  void removeFavouriteProduct([int? index, String? id]) {
+    final list = pagingController.value.itemList ?? [];
+    if (index != null) {
+      list.removeAt(index);
+    } else {
+      list.removeWhere((p) => p.id == id);
+    }
+    pagingController.value = PagingState(
+      itemList: list,
+      nextPageKey: list.length,
+    );
   }
 }
