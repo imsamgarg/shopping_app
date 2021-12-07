f7cf6b99619333401b01cfc3243114baa8398ee5
Sam Garg <smlgarg123@gmail.com>
2021-12-07 10:00:51 +0530

    appfuturebuilder added

---
+++ lib/app/core/global_widgets/future_builder.dart
@@ -0,0 +1,27 @@
+import 'package:custom_utils/future.dart';
+import 'package:flutter/material.dart';
+import 'package:shopping_app/app/core/global_widgets/widgets.dart';
+
+class AppFutureBuilder extends StatelessWidget {
+  final Future future;
+  final Widget Function(AsyncSnapshot snapshot) builder;
+  final Widget Function(AsyncSnapshot error)? errorBuilder;
+  final Widget? loading;
+  const AppFutureBuilder(
+      {Key? key,
+      required this.builder,
+      required this.future,
+      this.loading,
+      this.errorBuilder})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomFutureBuilder(
+      future: future,
+      builder: builder,
+      loading: loading ?? CenterLoading(size: 38),
+      errorBuilder: errorBuilder,
+    );
+  }
+}
