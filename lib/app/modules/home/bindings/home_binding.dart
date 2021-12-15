f2532896598e139a50be80885096d09c49a5b502
Sam Garg <smlgarg123@gmail.com>
2021-12-15 13:58:54 +0530

    bug fixes and controller added

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -2,8 +2,10 @@ import 'package:get/get.dart';
 
 import 'package:shopping_app/app/data/services/cart_service.dart';
 import 'package:shopping_app/app/data/services/config_service.dart';
+import 'package:shopping_app/app/data/services/products_service.dart';
 import 'package:shopping_app/app/data/services/user_service.dart';
 import 'package:shopping_app/app/modules/home/controllers/favourite_controller.dart';
+import 'package:shopping_app/app/modules/home/controllers/popular_products_controller.dart';
 import 'package:shopping_app/app/modules/home/controllers/profile_controller.dart';
 
 import '../controllers/home_controller.dart';
@@ -11,8 +13,12 @@ import '../controllers/home_controller.dart';
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<PopularProductsController>(
+      () => PopularProductsController(),
+    );
     //services
     Get.put<UserService>(UserService());
+    Get.put<ProductsService>(ProductsService());
     Get.put<CartService>(CartService());
     Get.put<ConfigService>(ConfigService());
 
---
+++ lib/app/modules/home/controllers/popular_products_controller.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:shopping_app/app/core/utils/mixins/routes_mixin.dart';
+import 'package:shopping_app/app/core/utils/mixins/services_mixin.dart';
+import 'package:shopping_app/app/data/models/product_model.dart';
+
+class PopularProductsController extends GetxController
+    with ServicesMixin, RoutesMixin {
+  late final PagingController<int, ProductSnapshot> pagingController;
+
+  @override
+  void onInit() {
+    pagingController = PagingController<int, ProductSnapshot>(firstPageKey: 0);
+    pagingController.addPageRequestListener(_getPopularProducts);
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    pagingController.dispose();
+  }
+
+  Future<void> _getPopularProducts(int pageKey) async {
+    QueryDocumentSnapshot<Map<String, dynamic>>? snapshots;
+    if (pageKey != 0) {
+      snapshots = pagingController.value.itemList?.last.snapshot;
+    }
+    final docs = await productsService.getPopularProducts(
+      startAfter: snapshots,
+    );
+    if (docs.length < pageKey) {
+      pagingController.appendLastPage(docs);
+    } else {
+      pagingController.appendPage(docs, pageKey + docs.length);
+    }
+  }
+}
