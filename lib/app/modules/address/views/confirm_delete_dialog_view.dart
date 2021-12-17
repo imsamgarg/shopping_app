b05ec01507d4caa906d34e457856753e9a2bb720
Sam Garg <smlgarg123@gmail.com>
2021-12-18 01:25:26 +0530

    confirm delete dialog added

---
+++ lib/app/modules/address/views/confirm_delete_dialog_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:shopping_app/app/core/global_widgets/buttons.dart';
+import 'package:shopping_app/app/core/theme/sizing_theme.dart';
+import 'package:shopping_app/app/modules/address/controllers/address_list_controller.dart';
+
+class ConfirmDeleteDialogView extends GetView<AddressListController> {
+  ConfirmDeleteDialogView(this.index);
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Confirm Delete"),
+      backgroundColor: Theme.of(context).dialogBackgroundColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: Sizing.borderRadiusS,
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Are You Sure Want To Delete?",
+          ),
+        ],
+      ),
+      actions: [
+        DialogButton(
+          onTap: () => Get.back(),
+          textColor: Theme.of(context).primaryColor,
+          heading: "No",
+        ),
+        GetBuilder<AddressListController>(
+          init: controller,
+          id: controller.deleteButtonId,
+          builder: (_) {
+            return DialogButton(
+              onTap: () => controller.confirmDelete(index),
+              color: Theme.of(context).primaryColor,
+              isLoading: controller.isButtonLoading,
+              heading: "Yes",
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
