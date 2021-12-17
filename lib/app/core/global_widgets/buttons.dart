e87d3aa532a5bcaf6f202e6094b9e36819e59dee
Sam Garg <smlgarg123@gmail.com>
2021-12-17 17:28:31 +0530

    buttons update

--- lib/app/core/global_widgets/buttons.dart
+++ lib/app/core/global_widgets/buttons.dart
@@ -69,42 +69,47 @@ class CallButton extends StatelessWidget {
 
 class AppTextButton extends StatelessWidget {
   final bool isLoading;
-  final Color? color;
+  final Color color;
   final Color? foregroundColor;
   final Widget child;
   final VoidCallback onTap;
+
+  final EdgeInsets? padding;
 
   const AppTextButton({
     Key? key,
     this.isLoading = false,
     required this.onTap,
-    this.color,
+    this.color = Vx.white,
     required this.child,
     this.foregroundColor,
+    this.padding,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      style: color != null
-          ? ButtonStyle(
-              backgroundColor: MaterialStateColor.resolveWith(
-                (states) {
-                  if (states.contains(MaterialState.pressed)) {
-                    return color!.withOpacity(0.7);
-                  }
-                  return color!;
-                },
-              ),
-              foregroundColor: MaterialStateColor.resolveWith(
-                (states) => foregroundColor ?? Vx.white,
-              ),
-            )
-          : TextButton.styleFrom(
-              // primary: Vx.white,
-              padding: const EdgeInsets.all(18),
-              shape: Sizing.roundShape,
-            ),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateColor.resolveWith(
+          (states) {
+            if (states.contains(MaterialState.pressed)) {
+              return color.withOpacity(0.7);
+            }
+            return color;
+          },
+        ),
+        foregroundColor: MaterialStateColor.resolveWith(
+          (states) => foregroundColor ?? Vx.white,
+        ),
+        padding: MaterialStateProperty.resolveWith(
+          (states) {
+            return padding ?? const EdgeInsets.all(18);
+          },
+        ),
+        shape: MaterialStateProperty.resolveWith((states) {
+          return Sizing.roundShape;
+        }),
+      ),
       onPressed: isLoading ? () {} : onTap,
       child: isLoading
           ? CenterLoading(
@@ -112,6 +117,38 @@ class AppTextButton extends StatelessWidget {
               color: foregroundColor,
             )
           : child,
+    );
+  }
+}
+
+class DialogButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final Color? color;
+  final Color textColor;
+  final String heading;
+  final bool isLoading;
+
+  const DialogButton({
+    Key? key,
+    required this.onTap,
+    this.color,
+    required this.heading,
+    this.isLoading = false,
+    this.textColor = Vx.white,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 80,
+      height: 40,
+      child: AppTextButton(
+        child: heading.text.color(textColor).make(),
+        padding: const EdgeInsets.all(0),
+        color: color ?? Vx.white,
+        isLoading: isLoading,
+        onTap: onTap,
+      ),
     );
   }
 }
