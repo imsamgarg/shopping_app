3abf44a130e8bd5f0db280e3bcb7784bb04701a7
Sam Garg <smlgarg123@gmail.com>
2021-12-27 18:41:17 +0530

    added shimmer.dart file

---
+++ lib/app/core/global_widgets/shimmer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/app/core/theme/sizing_theme.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class ShimmerContainer extends StatelessWidget {
+  const ShimmerContainer({
+    Key? key,
+    this.color,
+    this.borderRadius,
+  }) : super(key: key);
+
+  final Color? color;
+  final double? borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return VxShimmer(
+      child: SizedBox.expand(),
+      primaryColor: color ?? Colors.grey,
+    ).box.withRounded(value: borderRadius ?? Sizing.radiusL).make();
+  }
+}
+
+class ShimmerCard extends StatelessWidget {
+  const ShimmerCard({Key? key, this.axis = Axis.vertical}) : super(key: key);
+  final Axis axis;
+  @override
+  Widget build(BuildContext context) {
+    if (axis == Axis.horizontal) {
+      return _HorShimmerCard();
+    } else {
+      return _VerShimmerCard();
+    }
+  }
+}
+
+class _VerShimmerCard extends StatelessWidget {
+  const _VerShimmerCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _HorShimmerCard extends StatelessWidget {
+  const _HorShimmerCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
