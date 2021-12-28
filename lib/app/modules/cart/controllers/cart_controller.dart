b7b3c9e0d9cba378e4c73e091b3f3b980c40be7e
Sam Garg <smlgarg123@gmail.com>
2021-12-28 18:58:55 +0530

    place order logic done

--- lib/app/modules/cart/controllers/cart_controller.dart
+++ lib/app/modules/cart/controllers/cart_controller.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:shopping_app/app/core/utils/helper.dart';
+import 'package:shopping_app/app/core/utils/mixins/routes_mixin.dart';
 import 'package:shopping_app/app/core/utils/mixins/services_mixin.dart';
+import 'package:shopping_app/app/core/values/values.dart';
 import 'package:shopping_app/app/data/models/cart_model.dart';
+import 'package:shopping_app/app/modules/cart/controllers/cart_product_controller.dart';
 
-class CartController extends GetxController with ServicesMixin {
+class CartController extends GetxController with ServicesMixin, RoutesMixin {
   late final instance = _getCartProducts();
   late final List<CartModel> cartItems = [];
+
+  final invalidCartMessage = "Some Products are not available.";
+
+  late final cartProductsController = Get.find<CartProductController>();
 
   late final RxInt _finalPrice = calculateTotalPrice().obs;
   int get finalPrice => _finalPrice.value;
@@ -42,6 +50,34 @@ class CartController extends GetxController with ServicesMixin {
     }
   }
 
+  void updateTotalPrice() {
+    finalPrice = calculateTotalPrice();
+  }
+
+  void onPlaceOrder() {
+    final isValid = validateCartItems();
+
+    //dont do anything if cart is not valid
+    if (!isValid) return errorSnackbar(invalidCartMessage);
+
+    final checkoutModel = cartService.createCheckoutModel(cartItems);
+
+    //goto checkout view
+    onCheckoutTap(checkoutModel);
+  }
+
+  bool validateCartItems() {
+    for (var index = 0; index < cartItems.length; index++) {
+      final availablity = cartProductsController.checkAvailability(index);
+      if (availablity != ProductAvailability.available) {
+        return false;
+      }
+    }
+
+    //all clear
+    return true;
+  }
+}
   ///*No Longer Needed
 
   // void decrementQuantity(int index) {
@@ -63,8 +99,3 @@ class CartController extends GetxController with ServicesMixin {
   //   final id = item.product!.id!;
   //   cartService.incrementQuantity(id);
   // }
-
-  void updateTotalPrice() {
-    finalPrice = calculateTotalPrice();
-  }
-}
