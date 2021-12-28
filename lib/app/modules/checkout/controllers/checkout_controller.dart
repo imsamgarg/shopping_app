4738722d781fad08713ed4d52dce56ef1202e63c
Sam Garg <smlgarg123@gmail.com>
2021-12-28 18:29:45 +0530

    checkout_controller done

--- lib/app/modules/checkout/controllers/checkout_controller.dart
+++ lib/app/modules/checkout/controllers/checkout_controller.dart
@@ -1,17 +1,36 @@
 import 'package:get/get.dart';
 import 'package:shopping_app/app/core/values/values.dart';
+import 'package:shopping_app/app/data/models/cart_model.dart';
 import 'package:shopping_app/app/data/models/user_model.dart';
+import 'package:shopping_app/app/modules/checkout/views/checkout_loading_view.dart';
 
 class CheckoutController extends GetxController {
   static CheckoutController get to => Get.find<CheckoutController>();
+  late final List<CartModel> cartItems;
 
-  PaymentMethod? _paymentMethod;
+  late PaymentMethod _paymentMethod;
   late int totalPrice;
   late Address address;
 
   @override
   void onInit() {
+    populateFieldsFromArgument();
     super.onInit();
+  }
+
+  String get paymentMethod {
+    switch (_paymentMethod) {
+      case PaymentMethod.cashOnDelivery:
+        return "Cash On Delivery";
+      case PaymentMethod.prepaid:
+        return "Online Payment";
+    }
+  }
+
+  void populateFieldsFromArgument() {
+    final arguments = Get.arguments as CheckoutModel;
+    cartItems = arguments.cartItems;
+    totalPrice = arguments.totalPrice;
   }
 
   void updatePaymentMethod(paymentMethod) {
@@ -24,5 +43,9 @@ class CheckoutController extends GetxController {
 
   void updateAddress(Address _address) {
     address = _address;
+  }
+
+  void onConfirmOrder() {
+    Get.to(() => CheckoutLoadingView());
   }
 }
