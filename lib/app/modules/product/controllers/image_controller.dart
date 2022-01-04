c5d768acd7b32988c2fa978e4a9197a6fed7f9c7
Sam Garg <smlgarg123@gmail.com>
2022-01-04 16:50:55 +0530

    image controller done

--- lib/app/modules/product/controllers/image_controller.dart
+++ lib/app/modules/product/controllers/image_controller.dart
@@ -7,10 +7,67 @@ import 'package:shopping_app/app/modules/product/controllers/product_controller.
 class ProductImageController extends GetxController {
   String? image;
   late final productController = Get.find<ProductController>();
-  late final images = productController.images;
+  late final imageLength = images.length;
+  int currentImage = 0;
+
+  List<String?> get images {
+    return [...productController.images!, image];
+  }
+
   late final PageController pageController = PageController();
 
   void setSecondaryImage(String? img) {
+    if (img == null) {
+      return;
+    }
     image = img;
+    showLastImage();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void onInit() {
+    pageController.addListener(() {});
+    super.onInit();
+  }
+
+  void onLeftArrowPress() {
+    if (currentImage == 0) return;
+    int index = currentImage - 1;
+
+    return animateToPage(index);
+  }
+
+  void onRightArrowPress() {
+    if (currentImage == imageLength - 1 && images[imageLength - 1] != null) {
+      return;
+    }
+    int index = currentImage + 1;
+
+    return animateToPage(index);
+  }
+
+  void showLastImage() {
+    int index = imageLength - 1;
+    return animateToPage(index);
+  }
+
+  void animateToPage(int index) {
+    pageController.animateToPage(
+      index,
+      curve: Curves.easeInOut,
+      duration: const Duration(milliseconds: 400),
+    );
+
+    return updateCurrentImageIndex(index);
+  }
+
+  void updateCurrentImageIndex(int index) {
+    currentImage = index;
   }
 }
