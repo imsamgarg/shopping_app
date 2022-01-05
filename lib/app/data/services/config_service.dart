93c177652f15b2655b00c47b3eab5f66d884a638
Sam Garg <smlgarg123@gmail.com>
2022-01-06 00:13:57 +0530

    configservice refactor

--- lib/app/data/services/config_service.dart
+++ lib/app/data/services/config_service.dart
@@ -2,39 +2,49 @@ import 'package:get/get.dart';
 
 import 'package:shopping_app/app/core/values/db_strings.dart';
 import 'package:shopping_app/app/data/models/app_model.dart';
+import 'package:shopping_app/app/data/models/category_model.dart';
 import 'package:shopping_app/app/data/repository/database_repository.dart';
 
 class ConfigService extends GetxService {
   late final _repo = FirebaseDbRepository();
   static ConfigService service() => Get.find<ConfigService>();
-  // final Rxn<AppModel?> _data = Rxn<AppModel>(null);
-  // AppModel? get data => _data.value;
-  // set data(value) => _data.value = value;
 
   AppModel? data;
-  Future<void> init() async {
+  Future<ConfigService> initService() async {
     if (data == null) {
       await _getData();
-      // _subscribeData();
     }
-    return;
+    return this;
   }
 
-  // void _subscribeData() {
-  //   _repo.subscribeToDocument(Db.configDocument, Db.configCol).listen(
-  //     (event) {
-  //       final doc = event.data();
-  //       data = AppModel.fromJson(doc);
-  //     },
-  //   );
-  // }
+  Future getDoc(String docId) {
+    return _repo.getDocumentFromFirestore(docId, Db.configCol);
+  }
 
   Future<void> _getData() async {
-    final doc = await _repo.getDocumentFromFirestore(
-      Db.configDocument,
-      Db.configCol,
-    );
-    data = AppModel.fromJson(doc);
+    final _data = await Future.wait([
+      getDoc(Db.bannerDoc),
+      getDoc(Db.categoriesDoc),
+      getDoc(Db.subCategoriesDoc),
+      getDoc(Db.deliveryConfigDoc),
+    ]);
+
+    data = AppModel();
+
+    data!.banners = (_data[0] as List?)?.map((e) {
+      return AppBanner.fromJson(e);
+    }).toList();
+
+    data!.categories = (_data[1] as List?)?.map((e) {
+      return Category.fromJson(e);
+    }).toList();
+
+    data!.subCategories = (_data[2] as List?)?.map((e) {
+      return SubCategory.fromJson(e);
+    }).toList();
+
+    data!.deliveryConfig = DeliveryConfiguration.fromJson(_data[3]);
+
     return;
   }
 }
