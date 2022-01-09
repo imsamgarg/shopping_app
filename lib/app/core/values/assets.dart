ebba233e8c5897a269076db9dafda3d9c26a4b14
Sam Garg <smlgarg123@gmail.com>
2022-01-09 19:49:07 +0530

    assets added

--- lib/app/core/values/assets.dart
+++ lib/app/core/values/assets.dart
@@ -1,9 +1,41 @@
 class Assets {
   Assets._();
+  
+  /// Assets for assetsImagesEmptyIcon
+  /// assets/images/empty_icon.png
+  static const String assetsImagesEmptyIcon = "assets/images/empty_icon.png";
+
+  /// Assets for assetsImagesEmptyProductList
+  /// assets/images/empty_product_list.png
+  static const String assetsImagesEmptyProductList = "assets/images/empty_product_list.png";
+
+  /// Assets for assetsImagesEnterOtp
+  /// assets/images/enter_otp.png
+  static const String assetsImagesEnterOtp = "assets/images/enter_otp.png";
+
+  /// Assets for assetsImagesEnterOtpMain
+  /// assets/images/enter_otp_main.png
+  static const String assetsImagesEnterOtpMain = "assets/images/enter_otp_main.png";
+
+  /// Assets for assetsImagesErrorNotFound
+  /// assets/images/error_not_found.png
+  static const String assetsImagesErrorNotFound = "assets/images/error_not_found.png";
+
+  /// Assets for assetsImagesFavourite
+  /// assets/images/favourite.png
+  static const String assetsImagesFavourite = "assets/images/favourite.png";
 
   /// Assets for assetsImagesGLogo
   /// assets/images/gLogo.png
   static const String assetsImagesGLogo = "assets/images/gLogo.png";
+
+  /// Assets for assetsImagesLogin
+  /// assets/images/login.png
+  static const String assetsImagesLogin = "assets/images/login.png";
+
+  /// Assets for assetsImagesLoginMain
+  /// assets/images/login_main.png
+  static const String assetsImagesLoginMain = "assets/images/login_main.png";
 
   /// Assets for assetsImagesLogo
   /// assets/images/logo.png
@@ -11,8 +43,11 @@ class Assets {
 
   /// Assets for assetsImagesLoveAnimatiom
   /// assets/images/loveAnimatiom.riv
-  static const String assetsImagesLoveAnimatiom =
-      "assets/images/loveAnimatiom.riv";
+  static const String assetsImagesLoveAnimatiom = "assets/images/loveAnimatiom.riv";
+
+  /// Assets for assetsImagesRegister
+  /// assets/images/register.png
+  static const String assetsImagesRegister = "assets/images/register.png";
 
   /// Assets for assetsImagesShape2
   /// assets/images/shape2.png
@@ -21,4 +56,40 @@ class Assets {
   /// Assets for assetsImagesWelcome
   /// assets/images/welcome.png
   static const String assetsImagesWelcome = "assets/images/welcome.png";
+
+  /// Assets for assetsModelsAppModel
+  /// assets/models/app_model.json
+  static const String assetsModelsAppModel = "assets/models/app_model.json";
+
+  /// Assets for assetsModelsCart
+  /// assets/models/cart.json
+  static const String assetsModelsCart = "assets/models/cart.json";
+
+  /// Assets for assetsModelsCategories
+  /// assets/models/categories.json
+  static const String assetsModelsCategories = "assets/models/categories.json";
+
+  /// Assets for assetsModelsDynamicLink
+  /// assets/models/dynamic_link.json
+  static const String assetsModelsDynamicLink = "assets/models/dynamic_link.json";
+
+  /// Assets for assetsModelsFilterMap
+  /// assets/models/filter_map.json
+  static const String assetsModelsFilterMap = "assets/models/filter_map.json";
+
+  /// Assets for assetsModelsProductte
+  /// assets/models/productte.json
+  static const String assetsModelsProductte = "assets/models/productte.json";
+
+  /// Assets for assetsModelsSubcategories
+  /// assets/models/subcategories.json
+  static const String assetsModelsSubcategories = "assets/models/subcategories.json";
+
+  /// Assets for assetsModelsUser
+  /// assets/models/user.json
+  static const String assetsModelsUser = "assets/models/user.json";
+
+  /// Assets for assetsRealtimeDatabase
+  /// assets/realtime_database.json
+  static const String assetsRealtimeDatabase = "assets/realtime_database.json";
 }
