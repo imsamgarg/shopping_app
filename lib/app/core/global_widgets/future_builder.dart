7c30a99349d9f6f87facd989b5827ad30f199251
Sam Garg <smlgarg123@gmail.com>
2022-01-12 22:32:21 +0530

    default error added to future builder

--- lib/app/core/global_widgets/future_builder.dart
+++ lib/app/core/global_widgets/future_builder.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'package:custom_utils/future.dart';
+import 'package:get/route_manager.dart';
 
 import 'package:shopping_app/app/core/global_widgets/widgets.dart';
+import 'package:shopping_app/app/core/values/assets.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class AppFutureBuilder extends StatelessWidget {
   final Future future;
@@ -22,8 +25,20 @@ class AppFutureBuilder extends StatelessWidget {
     return CustomFutureBuilder(
       future: future,
       builder: builder,
-      loading: loading ?? const CenterLoading(size: 38),
-      errorBuilder: errorBuilder,
+      loading: loading ??
+          Container(
+            color: Vx.white,
+            child: const CenterLoading(size: 38),
+          ),
+      errorBuilder: errorBuilder ??
+          (_) {
+            return InfoWidget(
+              image: Assets.assetsImagesErrorNotFound,
+              message: "Something Went Wrong",
+              onTap: Get.back,
+              buttonMessage: "Go Back",
+            );
+          },
     );
   }
 }
